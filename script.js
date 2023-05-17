200c37242b6993788da32c2f16b79dc2b3c6a9b0
SamSamsung <Samuel.lelouch06@gmail.com>
2023-05-17 11:32:09 +0200

    Added a page to access the texts

---
+++ script.js
@@ -0,0 +1,26 @@
+function handleClick(event) {
+    event.target.classList.toggle("clicked");
+    
+  }
+
+
+  var L = ["Anneau de Gyges", "Bonheur au travail", "Cours Rousseau", "Critique du travail textes", "Diogene", "Droits de l'homme robespierre et tocqueville doc", "Jaures", "Le travail est le propre de l'homme", "Marx alienation", "Pierre Clastres", "Telephone portable", "Textes Bakounine Staline", "TEXTES ROUSSEAU", "Textes technique"]
+  window.addEventListener('load', (event) => {
+    titles = document.getElementsByClassName("title");
+    for(i=0; i< L.length; i++){
+        var p = document.createElement("p")
+        p.classList.add("title")
+        p.innerText = L[i]
+        
+        var a = document.createElement("a")
+        a.href = L[i].split("'").join(" ").split(" ").join("-") + ".pdf"
+        
+        var element = document.createElement("div")
+        element.classList.add("box");
+        element.onclick = handleClick
+        document.getElementsByClassName("contain")[0].appendChild(a)
+        a.appendChild(element)
+        element.appendChild(p)
+        /*titles[i].innerText = L[i].replace(" ", "-") + ".pdf"*/
+    }
+  });
